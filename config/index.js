e403d1af80a6a458ab213a5800662b8407df1dc7
Xuang12 <132531676+Xuang12@users.noreply.github.com>
2023-12-31 21:05:54 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -2,7 +2,7 @@ module.exports = {
   appid: "wxdddd77df919d3990",
   appsecret: "3f2b4262ea0b6f6926ac85b8942bfb5b",
   template_id: "N6Lhj8GXYiS_9uttvzXEZhQ88Pm-ti3fCeiTSgKPyOo",
-  touser: "oonWJ5slWuS2OrJdgCLM3GmNdxVk",
+  touser: "oqx1z6Soy_LnotoLjKVdCo89OnGI",
 
   key: "201c93c2e2b34bdeecdbf9f5265eb84e",
   city: "三明",
